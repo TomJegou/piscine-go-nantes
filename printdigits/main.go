cb179571334faadbeff46a8f54719e6d421b0643
tomrigolo <tomjegou10@gmail.com>
2022-09-07 10:37:54 +0200

    update program digit

--- printdigits/main.go
+++ printdigits/main.go
@@ -5,15 +5,9 @@ import (
 )
 
 func main() {
-	z01.PrintRune('0')
-	z01.PrintRune('1')
-	z01.PrintRune('2')
-	z01.PrintRune('3')
-	z01.PrintRune('4')
-	z01.PrintRune('5')
-	z01.PrintRune('6')
-	z01.PrintRune('7')
-	z01.PrintRune('8')
-	z01.PrintRune('9')
+	const DIGIT string = "0123456789"
+	for i := 0; i < 10; i++ {
+		z01.PrintRune(rune(DIGIT[i]))
+	}
 	z01.PrintRune('\n')
 }
