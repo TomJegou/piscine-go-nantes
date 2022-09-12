098159db3eb71e30a30a8a340e7eb836838f0ac3
jtom <tom.jegou@ynov.com>
2022-09-12 13:53:44 +0200

    add recursivepower.go v1

--- recursivepower.go
+++ recursivepower.go
@@ -1,1 +1,18 @@
 package piscine
+
+func RecursivePower(nb int, power int) int {
+	if power < 0 {
+		return 0
+	}
+	if nb == 1 || nb == 0 {
+		return nb
+	} else if power == 0 {
+		return 1
+	}
+	a := RecursivePower(nb, power/2)
+	if power%2 == 0 {
+		return a * a
+	} else {
+		return a * a * nb
+	}
+}
